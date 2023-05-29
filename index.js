4a47bff2072b380a1331a2fec54de5d1813b81d6
Muhammed Ansif V <muhammadansif9633@gmail.com>
2023-05-29 11:29:02 +0530

    Add Building Home component

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const autoGen_component = require('./autoGen_component.js');
 const Scaner_Componet = require('./Scaner_Componet.js');
 const cardLoder_Componet = require('./cardLoder_Componet.js');
 const navBar_Componet = require('./navBar_Componet.js');
+const HomeMinCard = require('./HomeMinCard.js');
+const HomeCard = require('./HomeCard.js');
 //__append_require_start
 module.exports = {
   components: {
@@ -9,6 +11,8 @@ module.exports = {
     Scaner_Componet,
     cardLoder_Componet,
     navBar_Componet,
+    HomeMinCard,
+    HomeCard,
     //__append_component
   },
   attributeTypes: {
